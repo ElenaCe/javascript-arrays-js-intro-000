35b625ca33503a2df65d5e804fb5293f72474cff
Elena Cellitti <elenacellitti92@gmail.com>
2019-08-08 10:31:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,6 @@ var chocolateBars =["snickers", "hundred grand", "kitkat", "skittles"];
 var a = ["how","are","you"]
 var e = ["Hello"]
 function addElementToBeginningOfArray(a,e){
-
+  a.unshift(e)
+  retrun a
 }
